caf3c84420b3660704d5c1c8dae8167cdbf85785
Chasen Bettinger <chasen.bettinger@deviq.io>
2019-12-30 16:10:53 -0500

    CRB: Add classname for Dots

--- src/components/CarouselDots.js
+++ src/components/CarouselDots.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import '../styles/CarouselDots.scss';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
+import "../styles/CarouselDots.scss";
 
 export default class CarouselDots extends Component {
   static propTypes = {
@@ -9,40 +9,47 @@ export default class CarouselDots extends Component {
     thumbnails: PropTypes.arrayOf(PropTypes.node),
     value: PropTypes.number,
     onChange: PropTypes.func,
+    className: PropTypes.string
   };
 
   onChange = index => () => {
     const numberOfSlides = this.props.number || this.props.thumbnails.length;
     const moduloItem = this.calculateButtonValue() % numberOfSlides;
 
-    return this.props.onChange(this.props.value - ( moduloItem - index));
+    return this.props.onChange(this.props.value - (moduloItem - index));
   };
 
   calculateButtonValue = () => {
     const numberOfSlides = this.props.number || this.props.thumbnails.length;
     return this.props.value >= 0
       ? this.props.value
-      : this.props.value + numberOfSlides * Math.ceil(Math.abs(this.props.value/numberOfSlides));
+      : this.props.value +
+          numberOfSlides *
+            Math.ceil(Math.abs(this.props.value / numberOfSlides));
   };
 
   renderCarouselDots() {
     if (this.props.thumbnails) {
-      const dotsLength = isNaN(this.props.number) ? this.props.thumbnails.length : this.props.number;
+      const dotsLength = isNaN(this.props.number)
+        ? this.props.thumbnails.length
+        : this.props.number;
 
-      return this.props.thumbnails.slice(0, dotsLength).map((thumbnail, index) => (
-        <li key={index}>
-          <div
-            className={classnames(
-              'BrainhubCarousel__thumbnail',
-              { 'BrainhubCarousel__thumbnail--selected': index === this.calculateButtonValue() % dotsLength }
-            )}
-            type="button"
-            onClick={this.onChange(index)}
-          >
-            {thumbnail}
-          </div>
-        </li>
-      ));
+      return this.props.thumbnails
+        .slice(0, dotsLength)
+        .map((thumbnail, index) => (
+          <li key={index}>
+            <div
+              className={classnames("BrainhubCarousel__thumbnail", {
+                "BrainhubCarousel__thumbnail--selected":
+                  index === this.calculateButtonValue() % dotsLength
+              })}
+              type="button"
+              onClick={this.onChange(index)}
+            >
+              {thumbnail}
+            </div>
+          </li>
+        ));
     }
 
     const dots = [];
@@ -50,10 +57,10 @@ export default class CarouselDots extends Component {
       dots.push(
         <li key={i}>
           <div
-            className={classnames(
-              'BrainhubCarousel__dot',
-              { 'BrainhubCarousel__dot--selected': i === this.calculateButtonValue() % this.props.number }
-            )}
+            className={classnames("BrainhubCarousel__dot", {
+              "BrainhubCarousel__dot--selected":
+                i === this.calculateButtonValue() % this.props.number
+            })}
             type="button"
             onClick={this.onChange(i)}
           >
@@ -67,7 +74,7 @@ export default class CarouselDots extends Component {
 
   render() {
     return (
-      <ul className="BrainhubCarousel__dots">
+      <ul className={`BrainhubCarousel__dots ${this.props.className || ""}`}>
         {this.renderCarouselDots()}
       </ul>
     );
